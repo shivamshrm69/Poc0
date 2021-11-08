88e508879537681df28c6d94304dd46734caf4d0
Shivam Sharma <uic.17mca8010@gmail.com>
2021-11-08 17:08:40 +0530

    housie

--- src/com/houseticket/HousieTicket.java
+++ src/com/houseticket/HousieTicket.java
@@ -4,32 +4,27 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-
 public class HousieTicket {
 	int col1, col2, col3, col4, col5, col6, col7, col8, col9 = 0;
 	static int row = 0;
-	 int row1,row2,row3 = 0;
+	int row1, row2, row3 = 0;
 	int inputCount = 0;
 	static Set<Integer> set = new HashSet<Integer>();
 
 	public void fillRowColumn(int num, int rowcol[][]) {
-	
+		try {
 			if (num >= 1 && num <= 10) {
 				int column1 = 0;
 				if (col1 == 2) {
 					System.out.println("Maximum Entry between 1 to 10");
 				} else {
-					if (rowcol[0][column1] == 0 && row1<5)
-					{
+					if (rowcol[0][column1] == 0 && row1 < 5) {
 						rowcol[0][column1] = num;
 						row1++;
-					}else if(rowcol[2][column1] == 0 && row3<5)
-					{
+					} else if (rowcol[2][column1] == 0 && row3 < 5) {
 						rowcol[2][column1] = num;
 						row3++;
-					}
-					else if (rowcol[1][column1] == 0 && row2<5)
-					{
+					} else if (rowcol[1][column1] == 0 && row2 < 5) {
 						rowcol[1][column1] = num;
 						row2++;
 					}
@@ -43,20 +38,15 @@ public class HousieTicket {
 				if (col2 == 2) {
 					System.out.println("Maximum Entry between 11 to 20");
 				} else {
-					 if (rowcol[1][column2] == 0 && row2<5)
-					{
+					if (rowcol[1][column2] == 0 && row2 < 5) {
 						rowcol[1][column2] = num;
 						row2++;
-					}
-					 else if (rowcol[0][column2] == 0  && row1<5)
-					{
+					} else if (rowcol[0][column2] == 0 && row1 < 5) {
 						rowcol[0][column2] = num;
 						row1++;
 					}
-					
-					
-					else if (rowcol[2][column2] == 0 && row3<5)
-					{
+
+					else if (rowcol[2][column2] == 0 && row3 < 5) {
 						rowcol[2][column2] = num;
 						row3++;
 					}
@@ -70,21 +60,17 @@ public class HousieTicket {
 				if (col3 == 2) {
 					System.out.println("Maximum Entry between 21 to 30");
 				} else {
-					if (rowcol[0][column3] == 0 && row1<5)
-					{
+					if (rowcol[0][column3] == 0 && row1 < 5) {
 						rowcol[0][column3] = num;
 						row1++;
-					}	else if (rowcol[2][column3] == 0 && row3<5)
-					{
+					} else if (rowcol[2][column3] == 0 && row3 < 5) {
 						rowcol[2][column3] = num;
 						row3++;
-					}
-					else if(rowcol[1][column3] == 0 && row2<5)
-					{
+					} else if (rowcol[1][column3] == 0 && row2 < 5) {
 						rowcol[1][column3] = num;
 						row2++;
 					}
-				
+
 					col3++;
 					row++;
 				}
@@ -95,19 +81,15 @@ public class HousieTicket {
 				if (col4 == 2) {
 					System.out.println("Maximum Entry For between 31 to 40");
 				} else {
-					 if(rowcol[1][column4] == 0 && row2<5)
-					{
+					if (rowcol[1][column4] == 0 && row2 < 5) {
 						rowcol[1][column4] = num;
 						row2++;
-					}
-					 else if (rowcol[0][column4] == 0 && row1<5)
-					{
+					} else if (rowcol[0][column4] == 0 && row1 < 5) {
 						rowcol[0][column4] = num;
 						row1++;
 					}
-					
-					else if (rowcol[2][column4] == 0 && row3<5)
-					{
+
+					else if (rowcol[2][column4] == 0 && row3 < 5) {
 						rowcol[2][column4] = num;
 						row3++;
 					}
@@ -122,23 +104,17 @@ public class HousieTicket {
 				if (col5 == 2) {
 					System.out.println("Maximum Entry between 41 to 50");
 				} else {
-					if (rowcol[0][column5] == 0 && row1<5)
-					{
+					if (rowcol[0][column5] == 0 && row1 < 5) {
 						rowcol[0][column5] = num;
 						row1++;
-					}
-					else if (rowcol[2][column5] == 0 && row3<5)
-					{
+					} else if (rowcol[2][column5] == 0 && row3 < 5) {
 						rowcol[2][column5] = num;
 						row3++;
-					}
-					else if (rowcol[1][column5] == 0 && row2<5)
-					{
+					} else if (rowcol[1][column5] == 0 && row2 < 5) {
 						rowcol[1][column5] = num;
 						row2++;
 					}
-				
-					
+
 					col5++;
 					row++;
 				}
@@ -149,23 +125,19 @@ public class HousieTicket {
 				if (col6 == 2) {
 					System.out.println("Maximum Entry between 51 to 60");
 				} else {
-					if (rowcol[2][column6] == 0 && row3<5)
-					{
+					if (rowcol[2][column6] == 0 && row3 < 5) {
 						rowcol[2][column6] = num;
 						row3++;
 					}
-				
-					else if (rowcol[1][column6] == 0 && row2<5)
-					{
+
+					else if (rowcol[1][column6] == 0 && row2 < 5) {
 						rowcol[1][column6] = num;
 						row2++;
-					}
-					else if (rowcol[0][column6] == 0 && row1<5)
-					{
+					} else if (rowcol[0][column6] == 0 && row1 < 5) {
 						rowcol[0][column6] = num;
 						row1++;
 					}
-					
+
 					col6++;
 					row++;
 				}
@@ -176,22 +148,17 @@ public class HousieTicket {
 				if (col7 == 2) {
 					System.out.println("Maximum Entry between 61 to 70");
 				} else {
-					if (rowcol[0][column7] == 0 && row1<5)
-					{
+					if (rowcol[0][column7] == 0 && row1 < 5) {
 						rowcol[0][column7] = num;
 						row1++;
-					}
-					else if (rowcol[2][column7] == 0 && row3<5)
-					{
+					} else if (rowcol[2][column7] == 0 && row3 < 5) {
 						rowcol[2][column7] = num;
 						row3++;
-					}
-					else if (rowcol[1][column7] == 0 && row2<5)
-					{
+					} else if (rowcol[1][column7] == 0 && row2 < 5) {
 						rowcol[1][column7] = num;
 						row2++;
 					}
-					
+
 					col7++;
 					row++;
 				}
@@ -202,20 +169,15 @@ public class HousieTicket {
 				if (col8 == 2) {
 					System.out.println("Maximum Entry between 71 to 80");
 				} else {
-					if (rowcol[1][column8] == 0 && row2<5)
-					{
+					if (rowcol[1][column8] == 0 && row2 < 5) {
 						rowcol[1][column8] = num;
 						row2++;
-					}
-					else if (rowcol[0][column8] == 0 && row1<5)
-					{
+					} else if (rowcol[0][column8] == 0 && row1 < 5) {
 						rowcol[0][column8] = num;
 						row1++;
 					}
-					
-					
-					else if (rowcol[2][column8] == 0 && row3<5)
-					{
+
+					else if (rowcol[2][column8] == 0 && row3 < 5) {
 						rowcol[2][column8] = num;
 						row3++;
 					}
@@ -229,18 +191,13 @@ public class HousieTicket {
 				if (col9 == 2) {
 					System.out.println("Maximum Entry between 81 to 90");
 				} else {
-					if (rowcol[0][column9] == 0 && row1<5)
-					{
+					if (rowcol[0][column9] == 0 && row1 < 5) {
 						rowcol[0][column9] = num;
 						row1++;
-					}
-					else if (rowcol[1][column9] == 0 && row2<5)
-					{
+					} else if (rowcol[1][column9] == 0 && row2 < 5) {
 						rowcol[1][column9] = num;
 						row2++;
-					}
-					else if (rowcol[2][column9] == 0 && row3<5)
-					{
+					} else if (rowcol[2][column9] == 0 && row3 < 5) {
 						rowcol[2][column9] = num;
 						row3++;
 					}
@@ -248,42 +205,45 @@ public class HousieTicket {
 					row++;
 				}
 			}
+		} catch (Exception e) {
+			System.out.println("Something went wrong please restart" + e.getMessage());
 		}
-	
+
+	}
 
 	public static void main(String[] args) {
 
 		int rowcol[][] = new int[3][9];
-		HousieTicket ht=new HousieTicket();
+		HousieTicket ht = new HousieTicket();
 		Scanner sc = new Scanner(System.in);
-
-		while (row != 15) {
-			System.out.println("Enter a Number between 1-90:");
-			int input = sc.nextInt();
-			if (input < 1 || input > 90) {
-				System.out.println("Invalid Number! Please Enter a Number between 1-90");
-			} else {
-				if(!set.contains(input))
-				{
-				set.add(input);
-				ht.fillRowColumn(input, rowcol);
-				}else
-				{
-					System.out.println("Duplicate Number!");
+		try {
+			while (row != 15) {
+				System.out.println("Enter a Number between 1-90:");
+				int input = sc.nextInt();
+				if (input < 1 || input > 90) {
+					System.out.println("Invalid Number! Please Enter a Number between 1-90");
+				} else {
+					if (!set.contains(input)) {
+						set.add(input);
+						ht.fillRowColumn(input, rowcol);
+					} else {
+						System.out.println("Duplicate Number!");
+					}
 				}
 			}
-		}
-		
-		if (row == 15) {
-			for (int i = 0; i < rowcol.length; i++) { 
-				for (int j = 0; j < rowcol[i].length; j++) { 
-	
-					System.out.print(rowcol[i][j] + " ");
+
+			if (row == 15) {
+				for (int i = 0; i < rowcol.length; i++) {
+					for (int j = 0; j < rowcol[i].length; j++) {
+
+						System.out.print(rowcol[i][j] + " ");
+					}
+					System.out.println();
 				}
-				System.out.println(); 
 			}
+
+		} catch (Exception e) {
+			System.out.println("Something went wrong please restart" + e.getMessage());
 		}
-
-
 	}
 }
